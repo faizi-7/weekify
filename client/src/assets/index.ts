6f213ee96525385b6c36cecf00a0ecd45df3e637
Vivek K <dearvivekji@gmail.com>
2024-10-22 17:05:34 +0000

    Added emoji sections and button

---
+++ client/src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as CloseButtonIcon } from './Icon.png';
+export { default as happy } from './happy.png';
+export { default as smiling } from './smiling.png';
+export { default as sad } from './sad.png';
+export { default as crying } from './crying.png';
